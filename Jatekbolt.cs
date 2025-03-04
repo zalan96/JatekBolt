5f4d1b75eedc710bc8bdbe4e1bd3f0b5fe55a09e
zalan96 <schzalan@gmail.com>
2025-03-04 08:56:24 +0100

    Add project files.

---
+++ Jatekbolt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace JatekBolt
+{
+	internal class Jatekbolt
+	{
+		public List<Jatek> Jatekok { get; private set; }
+		public List<Felhasznalo> Felhasznalok { get; private set; }
+		
+		public Jatekbolt()
+		{
+			Jatekok = new List<Jatek>();
+			Felhasznalok = new List<Felhasznalo>();
+		}
+
+		public void JatekHozzaadas(Jatek jatek)
+		{
+			Jatekok.Add(jatek);
+            Console.WriteLine("Játék sikeresen hozzáadva!");
+        }
+
+		public void FelhasznaloHozzaadas(Felhasznalo felhasznalo)
+		{
+			Felhasznalok.Add(felhasznalo);
+			Console.WriteLine("Felhasználó sikeresen hozzáadva!");
+		}
+
+		public Jatek KeresJatek(string cim)
+		{
+			foreach (var jatek in Jatekok)
+			{
+				if (jatek.cim == cim)
+				{
+					return jatek;
+				}
+			}
+			return null;
+		}
+
+		public Felhasznalo KeresFelhasznalo(string nev)
+		{
+			foreach (var felhasznalo in Felhasznalok)
+			{
+				if (felhasznalo.Nev == nev)
+				{
+					return felhasznalo;
+				}
+			}
+			return null;
+		}
+	}
+
+	
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace JatekBolt
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Jatekbolt jatekBolt = new Jatekbolt();
+			Felhasznalo adam = new Felhasznalo("Ádám", 20000);
+			jatekBolt.FelhasznaloHozzaadas(adam);
+			Jatek witcher = jatekBolt.KeresJatek("The Witcher 3");
+			adam.Vasarlas(witcher);
+			adam.Feltoltes(10000);
+			Jatek cyberpunk = jatekBolt.KeresJatek("Cyberpunk 2077");
+			adam.Vasarlas(cyberpunk);
+		}
+
+	}
+}
