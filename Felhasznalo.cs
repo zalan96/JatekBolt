b56d9b9fa03e2f107fb60e321be44f722fcfe5e0
zalan96 <schzalan@gmail.com>
2025-03-04 09:11:05 +0100

    done

---
+++ Felhasznalo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JatekBolt
+{
+	internal class Felhasznalo
+	{
+		public string Nev { get; private set; }
+		public int Egyenleg { get; private set; }
+		public List<Jatek> Konyvtar { get; private set; }
+
+
+		public Felhasznalo(string Nev, int Egyenleg)
+		{
+			this.Nev = Nev;
+			this.Egyenleg = Egyenleg;
+			Konyvtar = new List<Jatek>();
+		}
+
+		public bool Vasarlas(Jatek jatek)
+		{
+			if (jatek == null)
+			{
+				Console.WriteLine("A játék nem található.");
+				return false;
+			}
+
+			if (!Konyvtar.Contains(jatek) || Egyenleg > jatek.ar)
+			{
+				Konyvtar.Add(jatek);
+				Console.WriteLine($"{jatek} sikeresen megvásárolva!");
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("A játék nem található.");
+				return false;
+			}
+		}
+
+
+		public void KonyvtarMegtekintes()
+		{
+			Console.WriteLine("A játkok listája a könyvtárban:");
+			foreach (var jatek in Konyvtar)
+			{
+				Console.WriteLine(jatek.cim);
+			}
+		}
+
+		public void Feltoltes(int osszeg)
+		{
+			Egyenleg += osszeg;
+			Console.WriteLine($"Az egyenleg feltöltve: +{osszeg}Ft");
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,14 +4,10 @@
 	{
 		static void Main(string[] args)
 		{
-			Jatekbolt jatekBolt = new Jatekbolt();
 			Felhasznalo adam = new Felhasznalo("Ádám", 20000);
 			jatekBolt.FelhasznaloHozzaadas(adam);
 			Jatek witcher = jatekBolt.KeresJatek("The Witcher 3");
 			adam.Vasarlas(witcher);
-			adam.Feltoltes(10000);
-			Jatek cyberpunk = jatekBolt.KeresJatek("Cyberpunk 2077");
-			adam.Vasarlas(cyberpunk);
 		}
 
 	}
